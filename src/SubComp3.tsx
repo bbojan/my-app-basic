280fc91741b641e31cadb16dddd33782217b54c7
BojanB <ext_bojanB@playtika.com>
2021-02-09 09:48:08 +0100

    444444444444

--- src/SubComp3.tsx
+++ src/SubComp3.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 
 export const SubComp3 = (props: any) => {
   const space = props.getSpace();
+
+  const factor = useMemo(() => {
+    return factorialize(space);
+  }, [space]);
+
+  const b = 4;
+
+  const multiply = useCallback(
+    (a: number) => {
+      return a * b;
+    },
+    [b]
+  );
+
+  const c = multiply(factor);
+  console.log({ a: factor, b, c });
+
   return (
     <div>
       <span style={{ display: "inline-block", paddingRight: 10 }}>left</span>
@@ -13,6 +30,17 @@ export const SubComp3 = (props: any) => {
         {props.renderMiddle("AFTER")}
       </span>
       <span style={{ display: "inline-block", paddingLeft: 10 }}>right</span>
+      <span style={{ display: "inline-block", paddingLeft: 10 }}>
+        {factor.toFixed(0)}
+      </span>
     </div>
   );
 };
+
+function factorialize(num: number): number {
+  if (num < 0) return -1;
+  else if (num == 0) return 1;
+  else {
+    return num * factorialize(num - 1);
+  }
+}
